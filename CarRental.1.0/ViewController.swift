9cf0cb74630af224a6b7879d90469d4d90b22ffb
KINGD4LAL <hamzahdalal12@gmail.com>
2022-04-09 06:06:33 +0100

    Initial Commit

---
+++ CarRental.1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarRental.1.0
+//
+//  Created by Hamzah Dalal on 09/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
